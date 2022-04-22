f5f63fb3cab8c587cf62c14eeafd0623a89f3fc7
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-22 22:17:54 +0500

    Оптимальный способ метод sumOptimal через try catch если по стандарту выполняется sum и в нем косяк тогда sumLong

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,54 @@
 package com.company;
 
+import java.math.BigInteger;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Integer a = 2_000_000_000;
+        Integer b = 2_000_000_000;
 
-        long main = new Main().sumLong((long) (2 * Math.pow(10,18)), (long) (2 * Math.pow(10,18)));
-        System.out.println(main);
+        Main main = new Main();
+
+        System.out.println(main.sumOptimal(a,b));
+       // System.out.println(Math.pow(2,31));
 
 
 
     }
-    public int sum(int a, int b) {
-        return a + b;
+    public int sum(int a, int b) throws OutOfIntegerException {
+
+        int c = Integer.MAX_VALUE - a;
+        int d = b - c;
+
+        if (b >= c)
+            throw new OutOfIntegerException();
+        else
+            return a + b;
+
     }
 
     public long sumLong(long a, long b) {
         return (long) a + b;
+    }
+
+    public Number sumOptimal(int a, int b) {
+
+        try {
+            return sum(a,b);
+        }
+        catch (OutOfIntegerException exception) {
+            return sumLong(a,b);
+        }
+
+    }
+}
+
+class OutOfIntegerException extends Exception {
+
+    @Override
+    public String getMessage() {
+        return "out of type int";
     }
 }
