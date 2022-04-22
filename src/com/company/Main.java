9cb0a401cdb7e11a5b937ea256fcef845708f526
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-22 20:46:27 +0500

    Второй способ. Можно складвать миллиарды даже квантилионы (тип long) НО если например там всего 2 + 2 => будет не оптимальное решение так как long будет занимать 8 байт в стеке

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int main = new Main().sum(2,2);
+
+        long main = new Main().sumLong((long) (2 * Math.pow(10,18)), (long) (2 * Math.pow(10,18)));
+        System.out.println(main);
 
 
 
     }
     public int sum(int a, int b) {
         return a + b;
+    }
+
+    public long sumLong(long a, long b) {
+        return (long) a + b;
     }
 }
